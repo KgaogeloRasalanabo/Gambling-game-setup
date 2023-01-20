1345e37a3d6c50236422297d59af0359865b0634
Kgaogelo Ishmael Rasalanabo <118310020+KgaogeloRasalanabo@users.noreply.github.com>
2023-01-21 01:05:04 +0200

    Add files via upload

--- gambling_game.py
+++ gambling_game.py
@@ -8,41 +8,61 @@ import sys
 
 # user's choice input & validator
 def ques(acc, user_bal):
+
     num = input("Choose number from 1 to 5: ")
     # choice validator
-    for z in num:
-        if z not in [str(y) for y in (range(1, 6))]:
-            print("Invalid choose entered" '\n')
+    if num == '':
+        print("Invalid character entered" '\n')
+        ques(acc, user_bal)
+
+    # validates the first character entered by user and discard the rest
+    for z in num[0]:
+        if z[0] not in [str(y) for y in (range(1, 6))]:
+            print("Invalid number/character entered" '\n')
             ques(acc, user_bal)
-    playing_rounds(acc, user_bal, int(num))
+
+    playing_rounds(acc, user_bal, int(num[0]))
 
 
 # playing rounds input & validator
 def playing_rounds(owner_acc, user_acc, n):
     pla = input("play times: ")
+    if pla == '':
+        print("Invalid play time's entered" '\n')
+        playing_rounds(owner_acc, user_acc, n)
+
     for v in pla:
         if v not in [str(y) for y in (range(0, 10))]:
             print("Invalid play time's entered" '\n')
             playing_rounds(owner_acc, user_acc, n)
 
-    adjustments(owner_acc, user_acc, n, int(pla))
+    wager_validator(owner_acc, user_acc, n, int(pla))
 
 
-# probability adjustors, accounts calculator, choice & random number comparator
-def adjustments(acc_balance, user_bala, numb, play):
-    mul = input("wager per play: R ")
-    # wager validator
-    for w in mul:
+# wager input & validator
+def wager_validator(acc_balance2, user_bala2, numb2, play2):
+
+    stake = input("wager per play: R ")
+    if stake == '':
+        print("Invalid wager entered" '\n')
+        wager_validator(acc_balance2, user_bala2, numb2, play2)
+
+    for w in stake:
         if w not in [str(y) for y in (range(0, 10))]:
             print("Invalid wager entered" '\n')
-            adjustments(acc_balance, user_bala, numb, play)
+            wager_validator(acc_balance2, user_bala2, numb2, play2)
 
-    wage = int(mul)
+    probability_adjust(acc_balance2, user_bala2, numb2, play2, stake)
+
+
+# probability adjustors & random number generator
+def probability_adjust(acc_balance, user_bala, numb, play, stake2):
+    wage = int(stake2)
     given = [1, 2, 3, 4, 5]
     a = b = c = d = e = 100
     count = 0
 
-    # probability adjuster, generator & comparator for random number, based on owner's account balance
+    # probability adjuster & random number generator, based on owner's account balance
     while 0 <= count < play and wage > 0:
         count += 1
         if acc_balance <= wage:
@@ -70,13 +90,27 @@ def adjustments(acc_balance, user_bala, numb, play):
             e = adjust
         lucky_num = (random.choices(given, weights=(a, b, c, d, e), k=1))
 
-        # user's choice & lucky number comparator and accounts calculator
+        # user's choice & lucky number comparator
         for i in lucky_num:
 
             if user_bala < wage:
-                print('\n'"Insufficient balance.")
-                user_bb = int(input("load account or enter 0 to withdraw/ change wager: ")) + user_bala
-                ques(acc_balance, user_bb)
+                print('\n'"Insufficient balance. R", user_bala, "available")
+                user_bb = input("load account or enter 0 to withdraw/ change wager: ")
+
+                # user's account top up validator
+                for h in user_bb:
+                    if h not in [str(y) for y in (range(0, 10))]:
+                        print("Invalid amount entered" '\n')
+                        probability_adjust(acc_balance, user_bala, numb, play, wage)
+
+                if user_bb == '':
+                    user_bbb = user_bala
+
+                else:
+                    user_bbb = int(user_bb)
+                    user_bbb += user_bala
+
+                ques(acc_balance, user_bbb)
 
             elif numb == i and user_bala >= wage:
                 acc_balance -= wage
@@ -102,8 +136,15 @@ def communicator(account_balance, user_balance):
     if play_again == 'Y':
         ques(account_balance, user_balance)
 
-    # exit game and owner's account document updater
+    # initiates new player
     elif play_again == 'N':
+        print("user balance withdrawal: R", user_balance)
+        print("play responsibly, winners know when to quit")
+        print('\n' "Next player, Welcome")
+        new_player(account_balance)
+
+    # exit game and owner's account document updater
+    else:
         print("user's balance withdrawal: R", user_balance)
         print("game closing!!!")
         sys.stdout = open("gambling game.txt", 'w')
@@ -111,18 +152,16 @@ def communicator(account_balance, user_balance):
         sys.stdout.close()
         exit()
 
-    # initiates new player
-    else:
-        print("user balance withdrawal: R", user_balance)
-        print("play responsibly, winners know when to quit")
-        print('\n' "Next player, Welcome")
-        new_player(account_balance)
-
 
 # new player account loader and validator
 def new_player(owner_balance):
+    
     user_b = input("load account balance: R ")
     # user account balance validator
+    if user_b == '':
+        print("Invalid amount entered" '\n')
+        new_player(owner_balance)
+
     for x in user_b:
         if x not in [str(y) for y in (range(0, 10))]:
             print("Invalid amount entered" '\n')
